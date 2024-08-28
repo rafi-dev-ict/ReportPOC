3353bffa98ebf80d68b6ea19ed6d653053d5c912
Kazi Rafi <rafi@burobd.org>
2024-08-28 16:00:24 +0600

    Add project files.

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReportPOC.Services;
+using System.Net.Mime;
+
+namespace ReportPOC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private IReportService _reportService;
+
+        public ReportController(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        [HttpGet("{reportName}/{reportType}")]
+        public ActionResult Get(string reportName, string reportType)
+        {
+            var reportNameWithLang = reportName;
+            var reportFileByteString = _reportService.GenerateReportAsync(reportNameWithLang, reportType);
+            return File(reportFileByteString, MediaTypeNames.Application.Octet, getReportName(reportNameWithLang, reportType));
+        }
+
+
+        private string getReportName(string reportName, string reportType)
+        {
+            var outputFileName = reportName + ".pdf";
+
+            switch (reportType.ToUpper())
+            {
+                default:
+                case "PDF":
+                    outputFileName = reportName + ".pdf";
+                    break;
+                case "XLS":
+                    outputFileName = reportName + ".xls";
+                    break;
+                case "WORD":
+                    outputFileName = reportName + ".doc";
+                    break;
+            }
+
+            return outputFileName;
+        }
+
+    
+}
+}
---
+++ Services/ReportService.cs
@@ -0,0 +1,67 @@
+﻿using AspNetCore.Reporting;
+using ReportPOC.Models;
+using System.Reflection;
+using System.Text;
+
+namespace ReportPOC.Services
+{
+    public interface IReportService
+    {
+        byte[] GenerateReportAsync(string reportName, string reportType);
+    }
+
+    public class ReportService : IReportService
+    {
+        public byte[] GenerateReportAsync(string reportName, string reportType)
+        {
+            // Get the directory of the executing assembly (your application root)
+            string rootPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            // Construct the full path to the RDLC file
+            string rdlcFilePath = Path.Combine(rootPath, "ReportFiles", $"{reportName}.rdlc");
+
+            if (!File.Exists(rdlcFilePath))
+            {
+                throw new FileNotFoundException($"The report file was not found at path: {rdlcFilePath}");
+            }
+
+            // Register the necessary encoding provider
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            LocalReport report = new LocalReport(rdlcFilePath);
+
+            // Prepare data for the report
+            List<UserDto> userList = new List<UserDto>
+            {
+                new UserDto { FirstName = "jp", LastName = "jan", Email = "jp@gm.com", Phone = "+976666661111" },
+                new UserDto { FirstName = "jp2", LastName = "jan", Email = "jp2@gm.com", Phone = "+976666661111" },
+                new UserDto { FirstName = "முதல் பெயர்", LastName = "கடைசி பெயர்", Email = "jp3@gm.com", Phone = "+976666661111" },
+                new UserDto { FirstName = "पहला नाम", LastName = "अंतिम नाम", Email = "jp4@gm.com", Phone = "+976666661111" },
+                new UserDto { FirstName = "jp5", LastName = "jan", Email = "jp5@gm.com", Phone = "+976666661111" }
+            };
+
+            //report.AddDataSource("dsUsers", userList);
+
+            // Create a dictionary for parameters and add the required 'title' parameter
+            Dictionary<string, string> parameters = new Dictionary<string, string>
+            {
+                { "title", "Hello world" }
+            };
+
+            var result = report.Execute(GetRenderType(reportType), 1, parameters);
+           
+
+            return result.MainStream;
+        }
+
+        private RenderType GetRenderType(string reportType)
+        {
+            return reportType.ToUpper() switch
+            {
+                "XLS" => RenderType.Excel,
+                "WORD" => RenderType.Word,
+                _ => RenderType.Pdf
+            };
+        }
+    }
+}
