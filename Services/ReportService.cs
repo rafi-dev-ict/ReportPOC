edf96343b89edd6088f4d282128c64f2a8ad582e
Kazi Rafi <rafi@burobd.org>
2024-08-28 16:24:55 +0600

    updated

--- Services/ReportService.cs
+++ Services/ReportService.cs
@@ -12,13 +12,32 @@ namespace ReportPOC.Services
 
     public class ReportService : IReportService
     {
+
         public byte[] GenerateReportAsync(string reportName, string reportType)
         {
             // Get the directory of the executing assembly (your application root)
             string rootPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
             // Construct the full path to the RDLC file
-            string rdlcFilePath = Path.Combine(rootPath, "ReportFiles", $"{reportName}.rdlc");
+            //string rdlcFilePath = Path.Combine(rootPath, "ReportFiles", $"{reportName}.rdlc");
+
+            //string rdlcFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ReportFiles", "Report1.rdlc");
+            //string rdlcFilePath = @"F:\POC\ReportPOC\ReportFiles\Report1.rdlc";
+
+            string rdlcFilePath = "";
+            IConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"));
+            var root = builder.Build();
+            var ReportFilePath = root.GetSection("ReportFilePath").GetChildren();
+
+            foreach (var path in ReportFilePath.ToList())
+            {
+                if (path.Key == "rdlc") rdlcFilePath = path.Value + reportName + ".rdlc";
+            }
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Encoding.GetEncoding("utf-8");
+
 
             if (!File.Exists(rdlcFilePath))
             {
@@ -27,28 +46,19 @@ namespace ReportPOC.Services
 
             // Register the necessary encoding provider
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Encoding.GetEncoding("utf-8");
 
             LocalReport report = new LocalReport(rdlcFilePath);
 
-            // Prepare data for the report
-            List<UserDto> userList = new List<UserDto>
-            {
-                new UserDto { FirstName = "jp", LastName = "jan", Email = "jp@gm.com", Phone = "+976666661111" },
-                new UserDto { FirstName = "jp2", LastName = "jan", Email = "jp2@gm.com", Phone = "+976666661111" },
-                new UserDto { FirstName = "முதல் பெயர்", LastName = "கடைசி பெயர்", Email = "jp3@gm.com", Phone = "+976666661111" },
-                new UserDto { FirstName = "पहला नाम", LastName = "अंतिम नाम", Email = "jp4@gm.com", Phone = "+976666661111" },
-                new UserDto { FirstName = "jp5", LastName = "jan", Email = "jp5@gm.com", Phone = "+976666661111" }
-            };
+           
 
             //report.AddDataSource("dsUsers", userList);
 
             // Create a dictionary for parameters and add the required 'title' parameter
-            Dictionary<string, string> parameters = new Dictionary<string, string>
-            {
-                { "title", "Hello world" }
-            };
+            var report_parameters = new Dictionary<string, string> { { "CompileVersion", "v.4.7" } };
+            report_parameters.Add("title", "BURO Bangladesh");
 
-            var result = report.Execute(GetRenderType(reportType), 1, parameters);
+            var result = report.Execute(GetRenderType(reportType), 1, report_parameters);
            
 
             return result.MainStream;
